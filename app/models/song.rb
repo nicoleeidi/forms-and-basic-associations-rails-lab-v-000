0c7f687e586aded42b2aa25ba535575dbdc78851
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-19 20:37:19 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,11 +5,15 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     self.artist= Artist.find_or_create_by(name:name)
   end
+<<<<<<< HEAD
   def genre_name
     self.genre.name if self.genre
   #  self.try(:genre).try(:name)
   end
 
+=======
+<<<<<<< HEAD
+>>>>>>> 809084046aaec8b19971f29055ecb8923feb4f94
   def artist_name
     self.artist.name if self.artist
 #self.try(:artist).try(:name)
@@ -23,6 +27,11 @@ def note_contents=(notes)
       self.notes.build(content:note)
     end
   end
+=======
+def artist_name
+  self.artist.name
+end
+>>>>>>> a4794d87caca06c09023c25b421861c2718e8503
 end
 def note_contents
   newarray= self.notes.map do |note|
